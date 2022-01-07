7c2abe7d48c05799baa21fcfe921fcc5f623ff5d
caiomiyashiro <caiohenrique37@gmail.com>
2022-01-07 19:55:44 -0300

    airflow  project submission 2

--- 4_data_pipelines/airflow/plugins/operators/data_quality.py
+++ 4_data_pipelines/airflow/plugins/operators/data_quality.py
@@ -15,14 +15,15 @@ class DataQualityOperator(BaseOperator):
         super(DataQualityOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
         self.tables = tables
+        self.expected_result=[0,0]
 
     def execute(self, context):
         redshift_hook = PostgresHook(self.redshift_conn_id)
-        for table in self.tables:
+        for table, exp_result in zip(self.tables, self.expected_result):
             records = redshift_hook.get_records(f"SELECT COUNT(*) FROM {table}")
-            if len(records) < 1 or len(records[0]) < 1:
+            if len(records) == exp_result or len(records[0]) == exp_result:
                 raise ValueError(f"Data quality check failed. {table} returned no results")
             num_records = records[0][0]
-            if num_records < 1:
+            if num_records == exp_result:
                 raise ValueError(f"Data quality check failed. {table} contained 0 rows")
             self.log.info(f"Data quality on table {table} check passed with {records[0][0]} records")
